712d9740ebf318f6b7aee6f9420996b98ec3aa2a
Dipesh Shah <deepace780@gmail.com>
2022-08-15 21:57:04 +0545

    dashboard

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Button, CssBaseline, Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    console.log("logout clicked");
+    navigate("/login");
+  };
   return (
     <>
       <CssBaseline />
@@ -14,6 +20,15 @@ const Dashboard = () => {
           {/* static email and name */}
           <Typography variant="h5">Email:Dipesh@gmail.com</Typography>
           <Typography variant="h6">Name: Dipesh</Typography>
+          <Button
+            variant="contained"
+            color="warning"
+            size="large"
+            onClick={handleLogout}
+            sx={{ mt: 8 }}
+          >
+            Logout
+          </Button>
         </Grid>
         <Grid item sm={8}></Grid>
       </Grid>
